80f7fe7acddffae7657244bc79e6861561a3d80b
ubiquitous-aman <amankumaraman112@gmail.com>
2026-03-22 12:49:33 +0530

    clean repo

---
+++ api/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse #fixing the issue of file not being downloaded.
+from rembg import remove
+from fastapi.middleware.cors import CORSMiddleware #fixing the issue of different port(8000-uvicorn, 5500-loalhost)
+from PIL import Image
+import io
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.post("/remove-bg")
+async def remove_bg(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes))
+
+    output = remove(image)
+
+    buffer = io.BytesIO()
+    output.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    return StreamingResponse(buffer, media_type="image/png")
