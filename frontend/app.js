9c1ef34ba10028ab8e6396de0cc8ac589f323b9a
ubiquitous-aman <amankumaraman112@gmail.com>
2026-03-22 14:29:05 +0530

    3

--- frontend/app.js
+++ frontend/app.js
@@ -13,7 +13,7 @@ removeBtn.addEventListener("click", async () => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch("https://rmbg-production-8e45.up.railway.app/remove-bg", {
+  const response = await fetch("https://rm-bg-4twa.onrender.com/remove-bg", {
     method: "POST",
     body: formData,
   });
